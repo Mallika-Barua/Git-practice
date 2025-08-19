03d9b79fb06247b85234a63565e7741934700c26
Mallika Barua <137826230+Mallika-Barua@users.noreply.github.com>
2025-08-20 00:50:38 +0600

    revert fibonacci sequence program

--- basic_program.cpp
+++ basic_program.cpp
@@ -7,20 +7,6 @@ int factorial(int n) {
     return fact;
 }
 
-
-void fibonacci(int n) {
-    int a = 0, b = 1, c;
-    cout << "Fibonacci sequence: ";
-    for (int i = 0; i < n; i++) {
-        cout << a << " ";
-        c = a + b;
-        a = b;
-        b = c;
-    }
-    cout << endl;
-}
-
-
 int main() {
     int n;
     cout << "Enter a number: ";
@@ -29,3 +15,4 @@ int main() {
 
     return 0;
 }
+
